3746ebd024d1ee6af0c94a6d0d219a6d50b32439
Niyaz <niyazbans@icloud.com>
2024-05-22 23:33:02 +0300

    removed reduant template

--- src/vk/common.cc
+++ src/vk/common.cc
@@ -10,7 +10,7 @@ namespace extensions {
 std::vector<const char*> Get() {
   uint32_t ext_count = 0;
   const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&ext_count);
-  std::vector<const char*> extensions(glfw_extensions, glfw_extensions + ext_count);
+  std::vector extensions(glfw_extensions, glfw_extensions + ext_count);
 #ifdef DEBUG
   extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 #endif
